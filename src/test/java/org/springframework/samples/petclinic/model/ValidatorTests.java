fbdd731713ebe8952c3d40944674fc6dca777f97
nsgram <68147300+nsgram@users.noreply.github.com>
2022-11-11 19:50:05 +0530

    Update ValidatorTests.java
    Addde for test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("A");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
